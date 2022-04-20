2ff4a1cf77900f6ca3e60e12109956cba8e13d00
RicardoPasos <ricardoteposte@gmail.com>
2022-04-19 20:07:59 -0700

    Cambio de MARSS2 a MrTiendita en referencias.

--- MrTiendita/Vistas/FrmConfiguraciones.cs
+++ MrTiendita/Vistas/FrmConfiguraciones.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MARSS2.Controladores;
+using MrTiendita.Controladores;
 
 namespace MrTiendita.Vistas
 {
